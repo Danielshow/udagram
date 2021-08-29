b802eb6983da8cd719cb4dd63e16258407c0e207
Danielshow <danielshotonwa53@gmail.com>
2021-08-29 21:40:50 +0100

    Fix errors

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -1,13 +1,12 @@
 import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
-
+console.log(config.host, config.username, config.password, config.database);
 export const sequelize = new Sequelize({
   'username': config.username,
   'password': config.password,
   'database': config.database,
   'host': config.host,
-
   'dialect': config.dialect,
   'storage': ':memory:',
 });
