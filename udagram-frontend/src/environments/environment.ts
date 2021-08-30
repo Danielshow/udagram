a39f12525ee05482fbb74c7deed712cf6e9bd9ce
Danielshow <danielshotonwa53@gmail.com>
2021-08-30 12:17:25 +0100

    Add metrics sever

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://afc9c63eced294b648d962ba4f217f6e-741940088.us-west-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://ae252510efa8e4332a90cc15ede31df1-2053291956.us-west-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
